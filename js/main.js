d948ecd4a1193720f6060fe8937a8877ad748cac
jasoneczek <github@jasonschaufele.com>
2024-05-14 07:09:21 -0500

    Implement logic to display selected current and target languages in success modal message

--- js/main.js
+++ js/main.js
@@ -4,17 +4,27 @@ const modal = document.getElementById('success-modal');
 const currLang = document.getElementById('curr-lang');
 const targLang = document.getElementById('targ-lang');
 const closeBtn = document.getElementById('close-btn');
+const langChoice = document.getElementById('lang-choice');
 
 const handleSubmit = e => {
   e.preventDefault();
 
   article.classList.add('hidden');
   modal.style.display = 'flex';
+
+  displayLangSelects();
 };
 
 const closeModal = () => {
   article.classList.remove('hidden');
   modal.style.display = 'none';
+};
+
+const displayLangSelects = () => {
+  const curr = currLang.value;
+  const targ = targLang.value;
+
+  langChoice.innerHTML = `You have selected <span class="modal-lang">${curr}</span> as your current language and <span class="modal-lang">${targ}</span> as your target language. A language tutor will be in contact with you based on your selections.`;
 };
 
 form.addEventListener('submit', handleSubmit);
